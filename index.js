75ae4fa1943b77f3b2e1ce476262623dac72959c
usernamen7 <n7username7@gmail.com>
2016-12-20 15:07:13 +0200

    Add Joke

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ var TelegramBot = require('node-telegram-bot-api');
 
 var bot = new TelegramBot(token, { polling: true }); //неодноразове виконання
 
-var job = new Cron('0,30 * * * * * ', function() { //message every sec
-  console.log('Hello');
-});
-
-job.start();
-
 bot.on('message', function(msg) { //обробка повідомленнь
   var id = msg.chat.id;
   bot.sendMessage(id, msg.text);
-  //console.log(msg);
+  console.log(msg);
 })
+
+var job = new Cron('0,5 * * * * * ', function() { //message every sec
+  console.log('Hello');
+  var chatId = 220715725,
+      url = 'http://www.umori.li/api/random?site=bash.im&name=bash&num=2'
+
+  request(url, function (error, response, body) {
+    var data = JSON.parse(body);
+    bot.sendMessage(chatId, data[0].elementPureHtml);
+    //console.log(data);
+  })
+});
+
+job.start();
