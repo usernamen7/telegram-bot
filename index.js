bf8a7b00fe062d3e93d1a2289642747eedcbaac9
usernamen7 <n7username7@gmail.com>
2016-12-20 15:42:53 +0200

    babel, joke for any message

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-var TelegramBot = require('node-telegram-bot-api');
-    Cron = require('cron').CronJob,
-    request = require('request'),
+import TelegramBot from 'node-telegram-bot-api';
+import {CronJob} from 'cron';
+import request from 'request';
+import {AllHtmlEntities} from 'html-entities';
+
+var entities = new AllHtmlEntities(),
     token = '251474889:AAGEz5rQLAa-P-fdGtD9z77RdY49pf8I3tc';
+//not one time use
+var bot = new TelegramBot(token, { polling: true });
+//message processing
+bot.on('message', (msg) => {
+  var id = msg.from.id,
+  url = 'http://www.umori.li/api/random?site=bash.im&name=bash&num=1'
 
-var bot = new TelegramBot(token, { polling: true }); //неодноразове виконання
-
-bot.on('message', function(msg) { //обробка повідомленнь
-  var id = msg.chat.id;
-  bot.sendMessage(id, msg.text);
-  console.log(msg);
-})
-
-var job = new Cron('0,5 * * * * * ', function() { //message every sec
-  console.log('Hello');
-  var chatId = 220715725,
-      url = 'http://www.umori.li/api/random?site=bash.im&name=bash&num=2'
-
-  request(url, function (error, response, body) {
+  request(url, (error, response, body) => {
     var data = JSON.parse(body);
-    bot.sendMessage(chatId, data[0].elementPureHtml);
-    //console.log(data);
+    bot.sendMessage(id, entities.decode(data[0].elementPureHtml));
   })
 });
-
-job.start();
