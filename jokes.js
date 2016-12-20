e8293470614aa2cbaa87827648a8a0920aaac940
usernamen7 <n7username7@gmail.com>
2016-12-20 16:22:59 +0200

    install package

---
+++ jokes.js
@@ -0,0 +1,11 @@
+// "devDependencies": {
+//   "babel-core": "^6.14.0",
+//   "babel-plugin-add-module-exports": "^0.2.1",
+//   "babel-plugin-transform-object-assign": "^6.8.0",
+//   "babel-plugin-transform-runtime": "^6.15.0",
+//   "babel-preset-es2015": "^6.14.0",
+//   "gulp": "^3.9.1",
+//   "gulp-babel": "^6.1.2",
+//   "gulp-load-plugins": "^1.3.0",
+//   "gulp-watch": "^4.3.9"
+// }
